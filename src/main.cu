7fc1022cc756bbad0d99bfedd46614673dd89363
jdl2 <jlong29@gmail.com>
2022-02-14 11:04:34 -0500

    Fixed an odd bug in having cuda dim3 launch types declared in globals

--- src/main.cu
+++ src/main.cu
@@ -23,49 +23,40 @@
 //Reset Quad Tree state with new data
 static bool bReset      = false;
 
-//////////////////////
-// GLOBAL RESOURCES //
-//////////////////////
-struct GlobalResources
-{
-	//Problem Size
-	//Parameters
-	int N;
-	int W;
-	int H;
+// GLOBAL Variables //
+//Problem Size
+//Parameters
+int N;
+int W;
+int H;
 
-	int window_width;
-	int window_height;
-	float aspRat;
+int window_width;
+int window_height;
+float aspRat;
 
-	//CUDA
-	int threads;
-	int blocks;
+//CUDA
+int2*   h_noise;
+int2*   d_noise;
+size_t noiseSz;
 
-	float elpsTime;
-	cudaEvent_t start, stop;
+uchar4* d_img;
+size_t imgSz;
 
-	//2D
-	dim3 gridDim, blockDim;
+float elpsTime;
+cudaEvent_t start, stop;
 
-	int2*   h_noise;
-	int2*   d_noise;
-	uchar4* d_img;
+//OPENGL
+//Texture variables
+GLuint imageTex;
+struct cudaGraphicsResource *pcuImageRes;
 
-	//OPENGL
-	//Texture variables
-	GLuint imageTex;
-	struct cudaGraphicsResource *pcuImageRes;
-
-	// Timing Code
-	//OpenGL loop = GPU + host
-	StopWatchInterface *timer;
-	int fpsCount;        // FPS count for averaging
-	int fpsLimit;        // FPS limit for sampling
-	float avgFPS;
-	uint frameCount;
-};
-static GlobalResources g_res;
+// Timing Code
+//OpenGL loop = GPU + host
+StopWatchInterface *timer;
+int fpsCount;        // FPS count for averaging
+int fpsLimit;        // FPS limit for sampling
+float avgFPS;
+uint frameCount;
 
 /////////////////////////////////
 // OPENGL FORWARD DECLARATIONS //
@@ -87,46 +78,41 @@ static inline int divUp(int x, int y)
 
 int main(int argc, char** argv)
 {
-	//Parameters
-	int N = 16*1;
-	int W = 640;
-	int H = 480;
+	//Input Parameters
+	N = 16*16;
+	W = 640;
+	H = 480;
 
-	/////////////////
-	// GLOBAL DATA //
-	/////////////////
-	memset(&g_res, 0, sizeof(g_res));
-
-	//initialize timers
-	g_res.fpsCount   = 0;
-	g_res.fpsLimit   = 1;
-	g_res.avgFPS     = 0.0f;
-	g_res.frameCount = 0;
+	window_width  = W;
+	window_height = H;
+	aspRat        = (float)window_width/(float)window_height;
 
 	//Set buffer pointers to NULL
-	g_res.h_noise=NULL;
-	g_res.d_noise=NULL;
-	g_res.d_img=NULL;
-	
-	//Set Global data	
-	g_res.N             = N;
-	g_res.W             = W;
-	g_res.H             = H;
-	g_res.window_width  = W;
-	g_res.window_height = H;
-	g_res.aspRat        = (float)g_res.window_width/(float)g_res.window_height;
+	h_noise =NULL;
+	d_noise =NULL;
+	noiseSz = N*sizeof(int2);
+
+	d_img   =NULL;
+	imgSz   = W*H*sizeof(uchar4);
 
 	//Root GPU Launch Optimization
 	//1D
-	g_res.threads = NTHREADS;
-	g_res.blocks  = divUp(g_res.N, NTHREADS);
+	int threads = NTHREADS;
+	int blocks  = divUp(N, NTHREADS);
 
 	//2D
-	g_res.blockDim.x = WARPSIZE;
-	g_res.blockDim.y = NWARPS;
+	dim3 blockDim, gridDim;
+	blockDim.x = WARPSIZE;
+	blockDim.y = NWARPS;
 
-	g_res.gridDim.x  = divUp(g_res.W, g_res.blockDim.x);
-	g_res.gridDim.y  = divUp(g_res.H, g_res.blockDim.y);
+	gridDim.x  = divUp(W, blockDim.x);
+	gridDim.y  = divUp(H, blockDim.y);
+
+	//initialize timers
+	fpsCount   = 0;
+	fpsLimit   = 1;
+	avgFPS     = 0.0f;
+	frameCount = 0;
 
 	/* Set Up */
 	//Set Device
@@ -156,60 +142,60 @@ int main(int argc, char** argv)
 	}
 
 	// Create the CUTIL timer
-	sdkCreateTimer(&g_res.timer);
+	sdkCreateTimer(&timer);
 
 	//Allocate Memory
-	checkCudaErrors(cudaMallocHost((void **)&g_res.h_noise, sizeof(int2)*g_res.N));
-	checkCudaErrors(cudaMalloc((void **)&g_res.d_noise, sizeof(int2)*g_res.N));
-	checkCudaErrors(cudaMalloc((void **)&g_res.d_img, sizeof(uchar4)*g_res.W*g_res.H));
+	checkCudaErrors(cudaMallocHost((void **)&h_noise, noiseSz));
+	checkCudaErrors(cudaMalloc((void **)&d_noise, noiseSz));
+	checkCudaErrors(cudaMalloc((void **)&d_img, imgSz));
 
 	//Set Timers
-	checkCudaErrors(cudaEventCreate(&g_res.start));
-	checkCudaErrors(cudaEventCreate(&g_res.stop));
+	checkCudaErrors(cudaEventCreate(&start));
+	checkCudaErrors(cudaEventCreate(&stop));
 	
 	//Generate Random Data
-	checkCudaErrors(cudaEventRecord(g_res.start, 0));
+	checkCudaErrors(cudaEventRecord(start, 0));
 
 	int seed = (int)time(0);
 
-	generate_uniform2D_kernel<<<g_res.blocks, g_res.threads>>>(g_res.d_noise, seed, g_res.W, g_res.H, g_res.N);
+	generate_uniform2D_kernel<<<blocks, threads>>>(d_noise, seed, W, H, N);
 
-	checkCudaErrors(cudaEventRecord(g_res.stop, 0));
-	checkCudaErrors(cudaEventSynchronize(g_res.stop));
-	checkCudaErrors(cudaEventElapsedTime(&g_res.elpsTime, g_res.start, g_res.stop));
-	printf("\n\nElapsed time for random number generation:  %9.6f ms \n", g_res.elpsTime);
+	checkCudaErrors(cudaEventRecord(stop, 0));
+	checkCudaErrors(cudaEventSynchronize(stop));
+	checkCudaErrors(cudaEventElapsedTime(&elpsTime, start, stop));
+	printf("\n\nElapsed time for random number generation:  %9.6f ms \n", elpsTime);
 	
 	//Set Image to Black
-	// d_setBlackImag<<<g_res.gridDim, g_res.blockDim>>>(g_res.d_img, g_res.W, g_res.H);
-	checkCudaErrors(cudaMemset(g_res.d_img, 0, sizeof(uchar4)*g_res.W*g_res.H));
+	d_setBlackImag<<<gridDim, blockDim>>>(d_img, W, H);
+	// checkCudaErrors(cudaMemset(d_img, 0, sizeof(uchar4)*W*H));
 
 	//Write Random data onto image buffer
-	checkCudaErrors(cudaEventRecord(g_res.start, 0));
+	checkCudaErrors(cudaEventRecord(start, 0));
 
-	d_writeData2Image<<<g_res.blocks, g_res.threads>>>(g_res.d_img, g_res.d_noise, g_res.W, g_res.H, g_res.N);
+	d_writeData2Image<<<blocks, threads>>>(d_img, d_noise, W, H, N);
 
-	checkCudaErrors(cudaEventRecord(g_res.stop, 0));
-	checkCudaErrors(cudaEventSynchronize(g_res.stop));
-	checkCudaErrors(cudaEventElapsedTime(&g_res.elpsTime, g_res.start, g_res.stop));
-	printf("\n\nElapsed time for writing noise data:        %9.6f ms \n", g_res.elpsTime);
+	checkCudaErrors(cudaEventRecord(stop, 0));
+	checkCudaErrors(cudaEventSynchronize(stop));
+	checkCudaErrors(cudaEventElapsedTime(&elpsTime, start, stop));
+	printf("\n\nElapsed time for writing noise data:        %9.6f ms \n", elpsTime);
 
 	//Copy back to host for checking    
-	checkCudaErrors(cudaMemcpy(g_res.h_noise, g_res.d_noise, sizeof(int2)*g_res.N, cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaMemcpy(h_noise, d_noise, noiseSz, cudaMemcpyDeviceToHost));
 
 	printf("Printing out 2D Noise:\n\t");
-	for (int ii = 0; ii < min(100, g_res.N); ii++)
+	for (int ii = 0; ii < min(100, N); ii++)
 	{
-		printf("[%d, %d], ", g_res.h_noise[ii].x, g_res.h_noise[ii].y);
+		printf("[%d, %d], ", h_noise[ii].x, h_noise[ii].y);
 	}
 	printf("\n");
 
 	//convert device memory to texture
 	cudaArray_t ArrIm;
-	cudaGraphicsMapResources(1, &g_res.pcuImageRes, 0);
-	cudaGraphicsSubResourceGetMappedArray(&ArrIm, g_res.pcuImageRes, 0, 0);
+	cudaGraphicsMapResources(1, &pcuImageRes, 0);
+	cudaGraphicsSubResourceGetMappedArray(&ArrIm, pcuImageRes, 0, 0);
 
-	checkCudaErrors(cudaMemcpyToArray(ArrIm, 0, 0, g_res.d_img, g_res.W*g_res.H*sizeof(uchar4), cudaMemcpyDeviceToDevice));
-	cudaGraphicsUnmapResources(1, &g_res.pcuImageRes, 0);
+	checkCudaErrors(cudaMemcpyToArray(ArrIm, 0, 0, d_img, imgSz, cudaMemcpyDeviceToDevice));
+	cudaGraphicsUnmapResources(1, &pcuImageRes, 0);
 
 	////////////////////////////////
 	// LAUNCH OPENGL DISPLAY LOOP //
@@ -221,20 +207,20 @@ int main(int argc, char** argv)
 //OpenGL function definitions
 void computeFPS()
 {
-	g_res.frameCount++;
-	g_res.fpsCount++;
+	frameCount++;
+	fpsCount++;
 
-	if (g_res.fpsCount == g_res.fpsLimit)
+	if (fpsCount == fpsLimit)
 	{
-		g_res.avgFPS = 1.f / (sdkGetAverageTimerValue(&g_res.timer) / 1000.f);
-		g_res.fpsCount = 0;
-		g_res.fpsLimit = (int)MAX(g_res.avgFPS, 1.f);
+		avgFPS = 1.f / (sdkGetAverageTimerValue(&timer) / 1000.f);
+		fpsCount = 0;
+		fpsLimit = (int)MAX(avgFPS, 1.f);
 
-		sdkResetTimer(&g_res.timer);
+		sdkResetTimer(&timer);
 	}
 	
 	char fps[256];
-	sprintf(fps, "Quad Tree: %3.1f fps", g_res.avgFPS);
+	sprintf(fps, "Quad Tree: %3.1f fps", avgFPS);
 	glutSetWindowTitle(fps);
 }
 
@@ -244,7 +230,7 @@ bool initGL(int *argc, char **argv)
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
 
 	//Create Image Data Window
-	glutInitWindowSize(g_res.window_width, g_res.window_height);
+	glutInitWindowSize(window_width, window_height);
 	glutCreateWindow("Quad Tree");
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	glutReportErrors();
@@ -262,14 +248,14 @@ bool initGL(int *argc, char **argv)
 
 	// Create and Register OpenGL Texture for Image(1 channel)
 	cudaError_t err1;
-	glGenTextures(1, &g_res.imageTex);
-	glBindTexture(GL_TEXTURE_2D, g_res.imageTex);
+	glGenTextures(1, &imageTex);
+	glBindTexture(GL_TEXTURE_2D, imageTex);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, g_res.window_width, g_res.window_height, 0, GL_BGRA, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, window_width, window_height, 0, GL_BGRA, GL_UNSIGNED_BYTE, NULL);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	err1 = cudaGraphicsGLRegisterImage(&g_res.pcuImageRes, g_res.imageTex, GL_TEXTURE_2D, cudaGraphicsMapFlagsWriteDiscard);
+	err1 = cudaGraphicsGLRegisterImage(&pcuImageRes, imageTex, GL_TEXTURE_2D, cudaGraphicsMapFlagsWriteDiscard);
 
 	if (err1 != 0)
 	{
@@ -290,55 +276,69 @@ bool initGL(int *argc, char **argv)
 void display()
 {
 	//Time it
-	sdkStartTimer(&g_res.timer);
+	sdkStartTimer(&timer);
 
 	if (bReset)
 	{
 		bReset = false;
+
+		//Root GPU Launch Optimization
+		//1D
+		int threads = NTHREADS;
+		int blocks  = divUp(N, NTHREADS);
+
+		//2D
+		dim3 blockDim, gridDim;
+		blockDim.x = WARPSIZE;
+		blockDim.y = NWARPS;
+
+		gridDim.x  = divUp(W, blockDim.x);
+		gridDim.y  = divUp(H, blockDim.y);
+
 		//Generate Random Data
-		checkCudaErrors(cudaEventRecord(g_res.start, 0));
+		checkCudaErrors(cudaEventRecord(start, 0));
 
 		int seed = (int)time(0);
 
-		generate_uniform2D_kernel<<<g_res.blocks, g_res.threads>>>(g_res.d_noise, seed, g_res.W, g_res.H, g_res.N);
+		generate_uniform2D_kernel<<<blocks, threads>>>(d_noise, seed, W, H, N);
 
-		checkCudaErrors(cudaEventRecord(g_res.stop, 0));
-		checkCudaErrors(cudaEventSynchronize(g_res.stop));
-		checkCudaErrors(cudaEventElapsedTime(&g_res.elpsTime, g_res.start, g_res.stop));
-		printf("\n\nElapsed time for random number generation:  %9.6f ms \n", g_res.elpsTime);
+		checkCudaErrors(cudaEventRecord(stop, 0));
+		checkCudaErrors(cudaEventSynchronize(stop));
+		checkCudaErrors(cudaEventElapsedTime(&elpsTime, start, stop));
+		printf("\n\nElapsed time for random number generation:  %9.6f ms \n", elpsTime);
 		
 		//Set Image to Black
-		// d_setBlackImag<<<g_res.gridDim, g_res.blockDim>>>(g_res.d_img, g_res.W, g_res.H);
-		checkCudaErrors(cudaMemset(g_res.d_img, 0, sizeof(uchar4)*g_res.W*g_res.H));
+		d_setBlackImag<<<gridDim, blockDim>>>(d_img, W, H);
+		// checkCudaErrors(cudaMemset(d_img, 0, sizeof(uchar4)*W*H));
 
 		//Write Random data onto image buffer
-		checkCudaErrors(cudaEventRecord(g_res.start, 0));
+		checkCudaErrors(cudaEventRecord(start, 0));
 
-		d_writeData2Image<<<g_res.blocks, g_res.threads>>>(g_res.d_img, g_res.d_noise, g_res.W, g_res.H, g_res.N);
+		d_writeData2Image<<<blocks, threads>>>(d_img, d_noise, W, H, N);
 
-		checkCudaErrors(cudaEventRecord(g_res.stop, 0));
-		checkCudaErrors(cudaEventSynchronize(g_res.stop));
-		checkCudaErrors(cudaEventElapsedTime(&g_res.elpsTime, g_res.start, g_res.stop));
-		printf("\n\nElapsed time for writing noise data:        %9.6f ms \n", g_res.elpsTime);
+		checkCudaErrors(cudaEventRecord(stop, 0));
+		checkCudaErrors(cudaEventSynchronize(stop));
+		checkCudaErrors(cudaEventElapsedTime(&elpsTime, start, stop));
+		printf("\n\nElapsed time for writing noise data:        %9.6f ms \n", elpsTime);
 
 		//Copy back to host for checking    
-		checkCudaErrors(cudaMemcpy(g_res.h_noise, g_res.d_noise, sizeof(int2)*g_res.N, cudaMemcpyDeviceToHost));
+		checkCudaErrors(cudaMemcpy(h_noise, d_noise, noiseSz, cudaMemcpyDeviceToHost));
 
 		printf("Printing out 2D Noise:\n\t");
-		for (int ii = 0; ii < min(100, g_res.N); ii++)
+		for (int ii = 0; ii < min(100, N); ii++)
 		{
-			printf("[%d, %d], ", g_res.h_noise[ii].x, g_res.h_noise[ii].y);
+			printf("[%d, %d], ", h_noise[ii].x, h_noise[ii].y);
 		}
 		printf("\n");
 
 		//convert device memory to texture
 		cudaArray_t ArrIm;
-		cudaGraphicsMapResources(1, &g_res.pcuImageRes, 0);
-		cudaGraphicsSubResourceGetMappedArray(&ArrIm, g_res.pcuImageRes, 0, 0);
+		cudaGraphicsMapResources(1, &pcuImageRes, 0);
+		cudaGraphicsSubResourceGetMappedArray(&ArrIm, pcuImageRes, 0, 0);
 
 		//NOTE: DISTORTION MODEL: 3 * d_dstReSz
-		checkCudaErrors(cudaMemcpyToArray(ArrIm, 0, 0, g_res.d_img, g_res.W*g_res.H*sizeof(uchar4), cudaMemcpyDeviceToDevice));
-		cudaGraphicsUnmapResources(1, &g_res.pcuImageRes, 0);
+		checkCudaErrors(cudaMemcpyToArray(ArrIm, 0, 0, d_img, imgSz, cudaMemcpyDeviceToDevice));
+		cudaGraphicsUnmapResources(1, &pcuImageRes, 0);
 	}
 
 	/////////////////////////
@@ -349,7 +349,7 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT);
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-	glBindTexture(GL_TEXTURE_2D, g_res.imageTex);
+	glBindTexture(GL_TEXTURE_2D, imageTex);
 	glEnable(GL_TEXTURE_2D);
 	glDisable(GL_DEPTH_TEST);
 	glDisable(GL_LIGHTING);
@@ -358,13 +358,13 @@ void display()
 	//Note: texCoords go clockwise from top left and verts go counter-clockwise from lower left
 	glBegin(GL_QUADS);
 	glTexCoord2f(0.0, 1.0);
-	glVertex3f(-g_res.aspRat, -1.0, 0.0);
+	glVertex3f(-aspRat, -1.0, 0.0);
 	glTexCoord2f(1.0, 1.0);
-	glVertex3f(g_res.aspRat, -1.0, 0.0);
+	glVertex3f(aspRat, -1.0, 0.0);
 	glTexCoord2f(1.0, 0.0);
-	glVertex3f(g_res.aspRat, 1.0, 0.0);
+	glVertex3f(aspRat, 1.0, 0.0);
 	glTexCoord2f(0.0, 0.0);
-	glVertex3f(-g_res.aspRat, 1.0, 0.0);
+	glVertex3f(-aspRat, 1.0, 0.0);
 	glEnd();
 	glBindTexture(GL_TEXTURE_2D, 0);
 	glDisable(GL_TEXTURE_2D);
@@ -374,7 +374,7 @@ void display()
 	cudaDeviceSynchronize();
 
 	// Updating timing information
-	sdkStopTimer(&g_res.timer);
+	sdkStopTimer(&timer);
 	computeFPS();
 
 	//swap
@@ -412,21 +412,21 @@ void timerEvent(int value)
 
 void cleanup()
 {
-	sdkDeleteTimer(&g_res.timer);
+	sdkDeleteTimer(&timer);
 
 	// CUDA/OPENGL
 	fprintf(stdout,"\tCUDA:\n");
-	cudaGraphicsUnregisterResource(g_res.pcuImageRes);
-	glDeleteTextures(1, &g_res.imageTex);
-	g_res.imageTex = 0;
+	cudaGraphicsUnregisterResource(pcuImageRes);
+	glDeleteTextures(1, &imageTex);
+	imageTex = 0;
 	fprintf(stdout,"\t\tAll openGL resources cleaned\n");
 
 	//Deallocate memory
-	checkCudaErrors(cudaFreeHost(g_res.h_noise));
-	checkCudaErrors(cudaFree(g_res.d_noise));
-	checkCudaErrors(cudaFree(g_res.d_img));
+	checkCudaErrors(cudaFreeHost(h_noise));
+	checkCudaErrors(cudaFree(d_noise));
+	checkCudaErrors(cudaFree(d_img));
 
-	checkCudaErrors(cudaEventDestroy(g_res.start));
-	checkCudaErrors(cudaEventDestroy(g_res.stop));
+	checkCudaErrors(cudaEventDestroy(start));
+	checkCudaErrors(cudaEventDestroy(stop));
 	fprintf(stdout,"\t\tAll Cuda resources cleaned\n");
 }
